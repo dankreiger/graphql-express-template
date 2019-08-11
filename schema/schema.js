957ac91e70b32e45ec2ad92a0de34c749f021a18
Dan Kreiger <dan@dankreiger.com>
2019-08-11 03:01:56 +0200

    chore(models): resolve circular reference

--- schema/schema.js
+++ schema/schema.js
@@ -1,33 +1,47 @@
 const graphql = require('graphql');
 const axios = require('axios');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(
+          `http://localhost:3000/companies/${parentValue.id}/users`
+        );
+        return data;
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       async resolve(parentValue, args) {
-        console.log(parentValue, args);
         const { data } = await axios.get(
           `http://localhost:3000/companies/${parentValue.companyId}`
         );
         return data;
       }
     }
-  }
+  })
 });
 
 // used to allow graphql to enter into the application data graph
@@ -41,6 +55,17 @@ const RootQuery = new GraphQLObjectType({
         // used to return an actual piece of data
         const { data } = await axios.get(
           `http://localhost:3000/users/${args.id}`
+        );
+        return data;
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parentValue, args) {
+        // used to return an actual piece of data
+        const { data } = await axios.get(
+          `http://localhost:3000/companies/${args.id}`
         );
         return data;
       }
